3f2eb617f6b39b18ae65c5661350b52394fc078b
Lin Zhang <marshluca@gmail.com>
2012-07-13 23:57:42 +0800

    Create CBDemo/DetailViewController

---
+++ CBDemo/DetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.h
+//  CBDemo
+//
+//  Created by Lin Zhang on 12-7-13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (nonatomic, retain) IBOutlet UILabel *label;
+
+@end
