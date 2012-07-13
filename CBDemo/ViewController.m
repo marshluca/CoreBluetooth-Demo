3b5fda2de99bff16e900d012fe5d880bec42e685
Lin Zhang <marshluca@gmail.com>
2012-07-13 23:07:51 +0800

    comment the initial characteristics

--- CBDemo/ViewController.m
+++ CBDemo/ViewController.m
@@ -18,6 +18,7 @@
     BlueToothMe *instance = [BlueToothMe shared];
     [instance setDelegate:self];
     
+    /*
     NSArray *characteristics = [NSArray arrayWithObjects:[CBUUID UUIDWithString:@"2A1E"], 
                                                          [CBUUID UUIDWithString:@"2A1C"],
                                                          [CBUUID UUIDWithString:@"2A21"], nil];
@@ -28,6 +29,7 @@
     characteristics = [NSArray arrayWithObject:[CBUUID UUIDWithString:@"2A29"]];
                        
     [instance setCharacteristics:characteristics forServiceCBUUID:@"180A"];
+    */
     
     [instance hardwareResponse:^(CBPeripheral *peripheral, BLUETOOTH_STATUS status, NSError *error) {
         
